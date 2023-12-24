ee07347907d6f5016925853705d9f17393f684cc
Musab Al Mahi <musabalmahi53@gmail.com>
2023-12-24 13:41:13 +0600

    specific rooms section added successfully

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import { useSearchParams } from "react-router-dom";
 import Categories from "../../components/Categories/Categories";
 import Rooms from "../../components/Rooms/Rooms";
+import { Helmet } from "react-helmet-async";
 
 const Home = () => {
   const [params, setParams] = useSearchParams();
@@ -8,6 +9,9 @@ const Home = () => {
   console.log(category);
   return (
     <div>
+      <Helmet>
+        <title>StayVista | Vacation Homes & Condo Rentals</title>
+      </Helmet>
       {/* {Categories Section} */}
       <Categories></Categories>
       {/* {Rooms Section} */}
