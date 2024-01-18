796424974f69c25a622106c806b1a3c7db52e9c4
musabtheexpensive <musabalmahi53@gmail.com>
2024-01-18 14:28:02 +0600

    solve many-many route problem and user related route,data problem

--- client/src/components/Categories/CategoryBox.jsx
+++ client/src/components/Categories/CategoryBox.jsx
@@ -8,21 +8,18 @@ const CategoryBox = ({ label, icon: Icon, selected }) => {
     if (params) {
       currentQuery = qs.parse(params.toString());
     }
-      const updatedQuery = { ...currentQuery, category: label };
-      const url = qs.stringifyUrl({
-        url: "/",
-        query: updatedQuery,
-      });
-      navigate(url);
-    
+    const updatedQuery = { ...currentQuery, category: label };
+    const url = qs.stringifyUrl({
+      url: "/",
+      query: updatedQuery,
+    });
+    navigate(url);
   };
   return (
     <div
       onClick={handleClick}
       className={`flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer ${
-        selected
-          ? "border-b-neutral-800 text-neutral-800"
-          : "border-transparent text-neutral-500"
+        selected ? "border-b-neutral-800 text-neutral-800" : " " // border-transparent text-neutral-500
       }`}
     >
       <Icon size={26} />
